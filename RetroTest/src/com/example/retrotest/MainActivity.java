c0acfe17b1c5b5189577eee38bac20b579b355da
Mark Benner <goldfisho15@gmail.com>
2013-08-11 20:44:26 +1200

    Fixed stuff and did stuff
    -Finally found solution to inserting URL params
    -Is now loading 5 businesses each button press
    -Cleaned up a bunch of stuff

--- RetroTest/src/com/example/retrotest/MainActivity.java
+++ RetroTest/src/com/example/retrotest/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.retrotest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.RestAdapter;
-import retrofit.client.Request;
 import retrofit.http.GET;
 import retrofit.http.Headers;
-import retrofit.http.Path;
 import retrofit.http.Query;
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -14,41 +13,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private static final String API_URL = "http://api.ezypeezy.com/1";
-	private MainActivity local;
 	private API api;
 	private RestAdapter restAdapter;
 	private static int pageNum = 1;
-	private static String getQuery = "businesses?page=1&per=20";
-	public static String b = "1";
+	ArrayList<String> arrayList = new ArrayList<String>();
+	ArrayAdapter arrayAdapter;
 	
 	interface API {
 		@Headers({
 			"Authorization: Token token=350e87e94060be22ebbcc5ddb5c368f1",
 			"LOCATION: -36.910644,174.742481"
 		})
-		//@GET("/businesses?page=1&per=20")
 		@GET("/businesses")
-		
 	    Response<Business> listBusinesses(@Query("page")int pageNum, @Query("per")int perPage);
-		//Response<Business> listBusinesses();
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		local = this;
-		
-		
+		// Set up listView
+		ListView businessListView = (ListView)findViewById(R.id.listView1);
+        arrayAdapter = new ArrayAdapter(getBaseContext(), 
+        		android.R.layout.simple_list_item_1, arrayList); // arrayList will start empty
+        businessListView.setAdapter(arrayAdapter);
 	}
 	
 	 private class SetUpRetrofitTask extends AsyncTask<Void, Void, Void> {
+		List<Business> businesses;
         @Override
         protected Void doInBackground(Void... params) {
 	        // Debug the task thread name
@@ -56,17 +57,22 @@ public class MainActivity extends Activity {
 	         
 	        try {
 	        	api = restAdapter.create(API.class);  
-	        	//getQuery = "businesses?page=" + pageNum + "&per=20";
-	        	//getQuery = "businesses?page=1&per=20";
-	        	Response<Business> response = api.listBusinesses(pageNum, 20);	  
+	        	Response<Business> response = api.listBusinesses(pageNum, 5);	  
 	        	pageNum++;
-	        	List<Business> businesses = response.response;
+	        	businesses = response.response;
+	        	
 			    for (Business business : businesses) {
+			    	arrayList.add(business.name);
 			    	System.out.println("BUZZNESS: " + business.address);
 			    }
 	   		 } catch (Exception e) {System.out.println("Error: " + e.toString());}
 	         //
 	        return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+        	// Update listView with new businesses
+        	arrayAdapter.notifyDataSetChanged();
         }
  	}
 
@@ -86,18 +92,14 @@ public class MainActivity extends Activity {
 	        }       
 	    }
 	 
-	 public void MessageBox(String message)
-	    {
+	 public void MessageBox(String message) {
 	       Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	    } 
 	 
-	 protected void getMoreBusinesses()
-	 {
+	 protected void getMoreBusinesses() {
 		 restAdapter = new RestAdapter.Builder()
 	        .setServer(API_URL)
-	        .build();
-					
-			
+	        .build();								
 			SetUpRetrofitTask task = new SetUpRetrofitTask();	
 			task.execute();
 	 }
